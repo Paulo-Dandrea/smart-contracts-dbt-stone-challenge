82e400f7e4065a744c271de9fc74d466a84fd2c2
Paulo-Dandrea <prog.dandrea@gmail.com>
2022-04-24 12:49:26 -0300

    feat: analysis: the block with more gas used. With its tokens

---
+++ analyses/tokens_inside_block_with_more_gas.sql
@@ -0,0 +1,18 @@
+WITH ranked_by_gas AS (
+    SELECT
+        block_number,
+        block_gas_used,
+        token_address,
+        RANK() over(
+            ORDER BY
+                block_gas_used DESC
+        ) gas_rank
+    FROM
+        {{ ref('dim_blocks_inner_joined_by_tokens') }}
+)
+SELECT
+    *
+FROM
+    ranked_by_gas
+WHERE
+    gas_rank = 1
